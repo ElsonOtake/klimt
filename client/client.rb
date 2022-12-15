d403db454dc978e24a94a64c56fa520902f837c0
Elson Otake <elsonotake@yahoo.com.br>
2022-12-15 09:55:50 -0300

    Add default condition

--- client/client.rb
+++ client/client.rb
@@ -9,12 +9,15 @@ class Client
 
   # Your retrieve function plus any additional functions go here ...
   LIMIT = 10
-  def retrieve(option)
+  def retrieve(option = '')
     params = "?limit=#{LIMIT}"
-    page = option.has_key?(:page) && option[:page].is_a?(Integer) ? option[:page] : 1
-    params.concat("&offset=#{(option[:page] - 1) * 10}") unless page == 1
-    colors = option[:dominant_color] if option.has_key?(:dominant_color) && option[:dominant_color].is_a?(Array)
-    params.concat(colors.join('&dominant_color[]=').prepend('&dominant_color[]=')) if colors.all? { |clr| clr.is_a?(String) }
+    page = 1
+    if !option
+      page = option.has_key?(:page) && option[:page].is_a?(Integer) ? option[:page] : 1
+      params.concat("&offset=#{(option[:page] - 1) * 10}") unless page == 1
+      colors = option[:dominant_color] if option.has_key?(:dominant_color) && option[:dominant_color].is_a?(Array)
+      params.concat(colors.join('&dominant_color[]=').prepend('&dominant_color[]=')) if colors.all? { |clr| clr.is_a?(String) }
+    end
     response = JSON.parse(RestClient.get "#{ARTWORKS_URL}#{params}")
     return "No data found" if !response
 
@@ -33,14 +36,13 @@ class Client
     artist_name = []
     artist_id.each do |id|
       artist = JSON.parse(RestClient.get "#{ARTIST_URL}?id=#{id}")[0].transform_keys!(&:to_sym)
-      # artist.transform_keys!(&:to_sym)
       artist_name.push(artist[:name])
     end
     output = {}
     output[:id] = ids
     output[:for_sale] = for_sale
     output[:soldPrimaryCount] = sold_primary_count
-    output[:artistNames] = artist_name.sort!
+    output[:artistNames] = artist_name.sort
     output[:previousPage] = page == 1 ? nil : page - 1
     output[:nextPage] = page + 1
     output
@@ -49,4 +51,5 @@ class Client
 end
 
 client = Client.new
-p client.retrieve({ page: 2, dominant_color: ["red", "brown"] })
+p client.retrieve
+# p client.retrieve({ page: 2, dominant_color: ["red", "brown"] })
