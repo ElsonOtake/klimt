1eeb7ae737a01c380085ffc384b7d1e1798668e8
Elson Otake <elsonotake@yahoo.com.br>
2022-12-15 12:30:25 -0300

    Add response for empty results

--- client/client.rb
+++ client/client.rb
@@ -12,11 +12,14 @@ class Client
   def retrieve(option = '')
     params = "?limit=#{LIMIT}"
     page = 1
-    if !option
+    if option != ''
+      puts "option #{option}"
       page = option.has_key?(:page) && option[:page].is_a?(Integer) ? option[:page] : 1
       params.concat("&offset=#{(option[:page] - 1) * 10}") unless page == 1
       colors = option[:dominant_color] if option.has_key?(:dominant_color) && option[:dominant_color].is_a?(Array)
-      params.concat(colors.join('&dominant_color[]=').prepend('&dominant_color[]=')) if colors.all? { |clr| clr.is_a?(String) }
+      if colors
+        params.concat(colors.join('&dominant_color[]=').prepend('&dominant_color[]=')) if colors.all? { |clr| clr.is_a?(String) }
+      end
     end
     response = JSON.parse(RestClient.get "#{ARTWORKS_URL}#{params}")
     return "No data found" if !response
@@ -24,22 +27,24 @@ class Client
     ids = []
     for_sale = []
     sold_primary_count = 0
-    artist_id = []
-    response.each do |artwork|
-      artwork.transform_keys!(&:to_sym)
-      ids.push(artwork[:id])
-      artwork[:isPrimary] = ['red', 'blue', 'yellow'].include?(artwork[:dominant_color])
-      for_sale.push(artwork) if artwork[:availability] == 'for_sale'
-      sold_primary_count += 1 if artwork[:availability] == 'sold' && artwork[:isPrimary]
-      artist_id.push(artwork[:artist_id]) unless artist_id.include?(artwork[:artist_id])
-    end
     artist_name = []
-    artist_id.each do |id|
-      artist = JSON.parse(RestClient.get "#{ARTIST_URL}?id=#{id}")[0].transform_keys!(&:to_sym)
-      artist_name.push(artist[:name])
+    if response
+      artist_id = []
+      response.each do |artwork|
+        artwork.transform_keys!(&:to_sym)
+        ids.push(artwork[:id])
+        artwork[:isPrimary] = ['red', 'blue', 'yellow'].include?(artwork[:dominant_color]) if ['red', 'blue', 'yellow'].include?(artwork[:dominant_color])
+        for_sale.push(artwork) if artwork[:availability] == 'for_sale'
+        sold_primary_count += 1 if artwork[:availability] == 'sold' && artwork[:isPrimary]
+        artist_id.push(artwork[:artist_id]) unless artist_id.include?(artwork[:artist_id])
+      end
+      artist_id.each do |id|
+        artist = JSON.parse(RestClient.get "#{ARTIST_URL}?id=#{id}")[0].transform_keys!(&:to_sym)
+        artist_name.push(artist[:name])
+      end
     end
     output = {}
-    output[:id] = ids
+    output[:ids] = ids
     output[:for_sale] = for_sale
     output[:soldPrimaryCount] = sold_primary_count
     output[:artistNames] = artist_name.sort
@@ -51,5 +56,6 @@ class Client
 end
 
 client = Client.new
-p client.retrieve
+# p client.retrieve
+p client.retrieve({ page: 51 })
 # p client.retrieve({ page: 2, dominant_color: ["red", "brown"] })
